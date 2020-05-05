929567a108a946b84deb850e4a69dec366688437
YuzeZhang <zhangyuzechn@foxmail.com>
2020-05-05 23:47:58 +0800

    Add

---
+++ 072_offer_25_mergeTwoLists.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Zeno on 2020/5/5.
+//
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) {
+            return l2;
+        }
+        if (l2 == nullptr) {
+            return l1;
+        }
+
+        ListNode* head;
+        if (l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+
+        ListNode* cur = head;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                cur = cur->next;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                cur = cur->next;
+                l2 = l2->next;
+            }
+        }
+        cur->next = l1 == nullptr ? l2 : l1;
+        return head;
+    }
+};
